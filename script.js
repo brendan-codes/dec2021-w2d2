5ccf41dc5b569c113d8b9ee5d8c7d9bf968fe1e0
Brendan Stanton <bmsenate@gmail.com>
2021-12-21 16:05:19 -0800

    finish demo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+function removeElement(){
+    var element = document.querySelector("#join-btn");
+    element.remove();
+}
+
+function search(){
+    var element = document.querySelector("#searchbar");
+    alert(element.value);
+}
+
+function vote(id){
+    var element = document.querySelector(id);
+    var newText = parseInt(element.innerText) + 1;
+    console.log(newText);
+    element.innerText = newText;
+}
